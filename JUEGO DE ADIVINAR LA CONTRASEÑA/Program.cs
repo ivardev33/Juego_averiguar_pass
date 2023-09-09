89d16db05002ecd9ea1b0e3bcd7a95df0d5be667
Ivar <ivarrobot89@gmail.com>
2023-09-09 12:55:04 +0200

    Primer guardado juego de contraseñas

---
+++ JUEGO DE ADIVINAR LA CONTRASEÑA/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+
+
+        Console.WriteLine("Bievenido al juego de averiguar la clave");
+
+       
+        int nIntentos = 5;
+        string contraseñaCorrecta = "Contraseña1234";
+
+        
+        
+
+        while (nIntentos >0 ) 
+
+        {
+            Console.Write("Introduce la contraseña : ");
+            
+            string intento= Console.ReadLine();
+
+            if (intento == contraseñaCorrecta)
+            {
+
+                Console.WriteLine("Enhorabuena es correcta");
+                break;
+
+
+            }
+
+            else Console.WriteLine("Es incorrecta la constraseña lo siento");
+
+            nIntentos--;
+
+            if (nIntentos >0 )
+            {
+                Console.WriteLine($"Contraseña incorrecta te quedan {nIntentos} intentos");
+            }
+
+            else
+            {
+                Console.WriteLine("Game over, te has quedado sin intentos");
+            }
+        }
+
+
+
+
+    }
+}
